7e9ae7a52ac5367f7c99ba47158f69d49718b4a6
Mao-Nishikawa-Fukuyama <gc24014@anabuki-net.ne.jp>
2024-09-26 10:06:51 +0900

    kadai 2

--- 3DLv1_2024_x64vs2022/3DLv1_2024_x64vs2022/GameProgramming64/CApplication.cpp
+++ 3DLv1_2024_x64vs2022/3DLv1_2024_x64vs2022/GameProgramming64/CApplication.cpp
@@ -2,6 +2,7 @@
 #include "CApplication.h"
 //OpenGL
 #include "glut.h"
+#include "CVector.h"
 
 
 #define SOUND_BGM "res\\mario.wav" //BGM音声ファイル
@@ -27,37 +28,52 @@ void CApplication::Start()
 
 void CApplication::Update()
 {
+	//頂点1､頂点2､頂点3,法線データの作成
+	CVector v0, v1, v2, n;
+	//法線を上向きで設定する
+	n.Set(0.0f, 1.0f, 0.0f);
+	//頂点1の座標を設定する
+	v0.Set(0.0f, 0.0f, 0.5f);
+	//頂点2の座標を設定する
+	v1.Set(1.0f, 0.0f, 0.0f);
+	//頂点3の座標を設定する
+	v2.Set(0.0f, 0.0f, -0.5f);
+
 	//視点の設定
 	//gluLookAt(視点X, 視点Y, 視点Z, 中心X, 中心Y, 中心Z, 上向X, 上向Y, 上向Z)
 	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 	//描画開始
-	//glBegin(形)
-	//GL_TRIANGLES：三角形
 	glBegin(GL_TRIANGLES);
 
 	//法線（面の向き）の設定
-	//glNormal3f(X座標, Y座標, Z座標)
-	glNormal3f(0.0f, 1.0f, 0.0f);
-
+	glNormal3f(n.X(), n.Y(), n.Z());
 	//頂点座標の設定
-	//glVertex3f(X座標, Y座標, Z座標)
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, -0.5f);
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-	//面の向きはZ軸+方向
-	glNormal3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.0f, 0.0f);
+	//法線と頂点の設定
+	n.Set(0.0f, 0.0f, 1.0f);
+	v0.Set(0.5f, 0.0f, 0.0f);
+	v1.Set(0.0f, 1.0f, 0.0f);
+	v2.Set(-0.5f, 0.0f, 0.0f);
+	//三角形2の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-	//面の向きはX軸+方向
-	glNormal3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, -0.5f, 0.0f);
-
+	//法線と頂点の設定
+	n.Set(1.0f, 0.0f, 0.0f);
+	v0.Set(0.0f, 0.5f, 0.0f);
+	v1.Set(0.0f, 0.0f, 1.0f);
+	v2.Set(0.0f, -0.5f, 0.0f);
+	//三角形3の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 	//描画終了
 	glEnd();
 
