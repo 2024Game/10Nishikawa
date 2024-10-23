fdcfdfca0c09e463ba391ba49fe0d3bb364144fe
Mao-Nishikawa-Fukuyama <gc24014@anabuki-net.ne.jp>
2024-10-23 10:41:40 +0900

    kadai 11 Updated (CMatrix File add)

---
+++ 3DLv1_2024_x64vs2022/3DLv1_2024_x64vs2022/GameProgramming64/CMatrix.cpp
@@ -0,0 +1,33 @@
+#include "CMatrix.h"
+//標準入出力関数のインクルード
+#include <stdio.h>
+
+void CMatrix::Print()
+{
+	printf("%10f %10f %10f %10f\n",
+		mM[0][0], mM[0][1], mM[0][2], mM[0][3]);
+	printf("%10f %10f %10f %10f\n",
+		mM[1][0], mM[1][1], mM[1][2], mM[1][3]);
+	printf("%10f %10f %10f %10f\n",
+		mM[2][0], mM[2][1], mM[2][2], mM[2][3]);
+	printf("%10f %10f %10f %10f\n",
+		mM[3][0], mM[3][1], mM[3][2], mM[3][3]);
+}
+
+//デフォルトコンストラクタ
+CMatrix::CMatrix()
+{
+	Identity();
+}
+
+//単位行列の作成
+CMatrix CMatrix::Identity()
+{
+	//この行列を返す
+	
+	mM[0][0] = 1.0f, mM[0][1] = 0.0f, mM[0][2] = 0.0f, mM[0][3] = 0.0f;
+	mM[1][0] = 0.0f, mM[1][1] = 1.0f, mM[1][2] = 0.0f, mM[1][3] = 0.0f;
+	mM[2][0] = 0.0f, mM[2][1] = 0.0f, mM[2][2] = 1.0f, mM[2][3] = 0.0f;
+	mM[3][0] = 0.0f, mM[3][1] = 0.0f, mM[3][2] = 0.0f, mM[3][3] = 1.0f;
+	return *this;
+}
