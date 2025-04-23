51512edb0e29af0120a4ddcd12acd0e95a5478e5
Mao-Nishikawa-Fukuyama <gc24014@anabuki-net.ne.jp>
2025-04-23 09:49:41 +0900

    CModelX Add

---
+++ 3DLv2_x64vs2022/GameProgramming64/CModelX.cpp
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <string.h>
+#include "CModelX.h"
+#include "glut.h"
+#include <ctype.h>	//isspace関数の宣言
+
+
+
+/*
+GetToken
+文字列データから、単語を1つ取得する
+*/
+char* CModelX::GetToken()
+{
+	char* p = mpPointer;
+	char* q = mToken;
+	//タブ(\t)空白( )改行(\r)(\n)，；”の区切り文字以外になるまで読み飛ばす
+	while (*p != '\0' && IsDelimiter(*p)) p++;
+	if (*p == '{' || *p == '}')
+	{
+		//{または}ならmTokenに代入し次の文字へ
+		*q++ = *p++;
+	}
+	else
+	{
+		//タブ(\t)空白( )改行(\r)(\n)，；”の区切り文字、
+		//または、}の文字になるまでmTokenに代入する
+		while (*p != '\0' && !IsDelimiter(*p) && *p != '}')
+			*q++ = *p++;
+	}
+
+	*q = '\0';	//mTokenの最後に\0を代入
+	mpPointer = p;	//次の読み込むポイントを更新する
+
+	//もしmTokenが//の場合は、コメントなので改行まで読み飛ばす
+	/*
+	strcmp(文字列1, 文字列2)
+	文字列1と文字列2が等しい場合、0を返します。
+	文字列1と文字列2が等しくない場合、0以外を返します。
+	*/
+	if (!strcmp("//", mToken))
+	{
+		//改行まで読み飛ばす
+		while (*p != '\0' && !strchr("\r\n", *p)) p++;
+		//読み込み位置の更新
+		mpPointer = p;
+		//単語を取得する（再帰呼び出し）
+		return GetToken();
+	}
+	return mToken;
+}
+
+CModelX::CModelX()
+	: mpPointer(nullptr)
+{
+	//mTokenを初期化
+	memset(mToken, 0, sizeof(mToken));
+}
+
+CModelX::~CModelX()
+{
+	if (mFrame.size() > 0)
+	{
+		delete mFrame[0];
+	}
+}
+
+/*
+SkipNode
+ノードを読み飛ばす
+*/
+void CModelX::SkipNode()
+{
+	//文字が終わったら終了
+	while (*mpPointer != '\0')
+	{
+		GetToken();	//次の単語取得
+		//{が見つかったらループ終了
+		if (strchr(mToken, '{')) break;
+	}
+	int count = 1;
+	//文字が終わるか、カウントが0になったら終了
+	while (*mpPointer != '\0' && count > 0)
+	{
+		GetToken();	//次の単語取得
+		//{を見つけるとカウントアップ
+		if (strchr(mToken, '{')) count++;
+		//}を見つけるとカウントダウン
+		else if (strchr(mToken, '}')) count--;
+	}
+}
+
+void CModelX::Load(const char* file)
+{
+	//
+	//ファイルサイズを取得する
+	//
+	FILE* fp;	//ファイルポインタ変数の作成
+	fp = fopen(file, "rb");	//ファイルをオープンする
+	if (fp == NULL)
+	{	//エラーチェック
+		printf("fopen error:%s￥n", file);
+		return;
+	}
+	//ファイルの最後へ移動
+	fseek(fp, 0L, SEEK_END);
+	//ファイルサイズの取得
+	int size = ftell(fp);
+	//ファイルサイズ+1バイト分の領域を確保
+	char* buf = mpPointer = new char[size + 1];
+	//
+	//ファイルから3Dモデルのデータを読み込む
+	//
+	//ファイルの先頭へ移動
+	fseek(fp, 0L, SEEK_SET);
+	//確保した領域にファイルサイズ分データを読み込む
+	fread(buf, size, 1, fp);
+
+	//printf(buf);
+
+	//最後に\0を設定する（文字列の終端）
+	buf[size] = '\0';
+	fclose(fp);	//ファイルをクローズする
+
+	//文字列の最後まで繰り返し
+	while (*mpPointer != '\0')
+	{
+		GetToken();	//単語の取得
+		//単語がFrameの場合
+		if (strcmp(mToken, "Frame") == 0)
+		{
+			printf("%s ", mToken);	//Frame出力
+			GetToken();	//Frame名を取得
+			printf("%s\n", mToken); //Frame名を出力
+		}
+	}
+
+	SAFE_DELETE_ARRAY(buf);	//確保した領域を開放する
+}
+
+/*
+* IsDelimiter(c)
+* cが\t \r \n スペースなどの空白文字
+* または,;"などの文字であれば
+* 区切り文字としてtrueを返す
+*/
+bool CModelX::IsDelimiter(char c)
+{
+	//isspace(c)
+	//cが空白文字なら0以外を返す
+	if (isspace(c) != 0)
+		return true;
+	/*
+	strchr(文字列, 文字)
+	文字列に文字が含まれていれば、
+	見つかった文字へのポインタを返す
+	見つからなかったらNULLを返す
+	*/
+	if (strchr(",;\"", c) != NULL)
+		return true;
+	//区切り文字ではない
+	return false;
+}
+
+/*
+ CModelXFrame
+ model：CModelXインスタンスへのポインタ
+ フレームを作成する
+ 読み込み中にFrameが見つかれば、フレームを作成し、
+ 子フレームに追加する
+*/
+CModelXFrame::CModelXFrame(CModelX* model)
+	: mpName(nullptr)
+	, mIndex(0)
+{
+	//現在のフレーム配列の要素数を取得し設定する
+	mIndex = model->mFrame.size();
+	//CModelXのフレーム配列に追加する
+	model->mFrame.push_back(this);
+	//変換行列を単位行列にする
+	mTransformMatrix.Identity();
+	//次の単語（フレーム名の予定）を取得する
+	model->GetToken(); // frame name
+	//フレーム名分エリアを確保する
+	mpName = new char[strlen(model->mToken) + 1];
+	//フレーム名をコピーする
+	strcpy(mpName, model->mToken);
+	//次の単語（{の予定）を取得する
+	model->GetToken();  // {
+	//文字が無くなったら終わり
+	while (*model->mpPointer != '\0')
+	{
+		//次の単語取得
+		model->GetToken(); // Frame
+		//}かっこの場合は終了
+		if (strchr(model->mToken, '}')) break;
+		//新なフレームの場合は、子フレームに追加
+		if (strchr(model->mToken, '{'))			//課題３
+		{
+			//フレームを作成し、子フレームの配列に追加
+			mChild.push_back(new CModelXFrame(model));
+		}
+		else
+		{
+			//上記以外の要素は読み飛ばす
+			model->SkipNode();
+		}
+	}
+	//デバッグバージョンのみ有効
+#ifdef _DEBUG
+	printf("%s\n", mpName);
+#endif
+}
+
+CModelXFrame::~CModelXFrame()
+{
+	//子フレームを全て解放する
+	std::vector<CModelXFrame*>::iterator itr;
+	for (itr = mChild.begin(); itr != mChild.end(); itr++)
+	{
+		delete* itr;
+	}
+	//名前のエリアを解放する
+	SAFE_DELETE_ARRAY(mpName);
+}
---
+++ 3DLv2_x64vs2022/GameProgramming64/CModelX.h
@@ -0,0 +1,52 @@
+#ifndef CMODELX_H
+#define CMODELX_H
+
+#include <vector>	//vectorクラスのインクルード（動的配列）
+#include "CMatrix.h"	//マトリクスクラスのインクルード
+class CModelX;		// CModelXクラスの宣言
+class CModelXFrame;	// CModelXFrameクラスの宣言
+
+#define MODEL_FILE "res\\sample.blend.x"  //入力ファイル名
+//領域開放をマクロ化
+#define SAFE_DELETE_ARRAY(a) {if(a) delete[] a; a = nullptr;}
+/*
+CModelX
+Xファイル形式の3Dモデルデータをプログラムで認識する
+*/
+
+class CModelX
+{
+	friend CModelXFrame;
+public:
+	CModelX();
+	~CModelX();
+	//ノードの読み飛ばし
+	void SkipNode();
+	//単語の取り出し
+	char* GetToken();
+	void Load(const char* file);
+
+private:
+	std::vector<CModelXFrame*> mFrame;  //フレームの配列
+	char* mpPointer;
+	char mToken[1024];
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
+};
+
+class CModelXFrame
+{
+	friend CModelX;
+public:
+	//コンストラクタ
+	CModelXFrame(CModelX* model);
+	//デストラクタ
+	~CModelXFrame();
+private:
+	std::vector<CModelXFrame*> mChild;  //子フレームの配列
+	CMatrix mTransformMatrix;  //変換行列
+	char* mpName;   //フレーム名前
+	int mIndex;  //フレーム番号
+};
+#endif // !CMODELX_H
+
