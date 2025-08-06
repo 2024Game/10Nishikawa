eaeba3510551351fd9059362bcbfdf5d3ed04220
Mao-Nishikawa-Fukuyama <gc24014@anabuki-net.ne.jp>
2025-08-06 14:26:45 +0900

    kadai 30 Updated

--- 3DLv2_x64vs2022/GameProgramming64/CXPlayer.cpp
+++ 3DLv2_x64vs2022/GameProgramming64/CXPlayer.cpp
@@ -10,67 +10,9 @@ CXPlayer::CXPlayer()
 
 void CXPlayer::Update()
 {
-	//カメラの前方
-	CVector cameraZ = CActionCamera::Instance()->VectorZ();
-	//カメラの左方向
-	CVector cameraX = CActionCamera::Instance()->VectorX();
-	//キャラクタの前方
-	CVector charZ = mMatrixRotate.VectorZ();
-	//XZ平面にして正規化
-	cameraZ.Y(0.0f); cameraZ = cameraZ.Normalize();
-	cameraX.Y(0.0f); cameraX = cameraX.Normalize();
-	charZ.Y(0.0f); charZ = charZ.Normalize();
-	//移動方向の設定
-	CVector move;
-	if (mInput.Key('A'))
-	{
-		move = move + cameraX;
-	}
-	if (mInput.Key('D'))
-	{
-		move = move - cameraX;
-	}
-	if (mInput.Key('W'))
-	{
-		move = move + cameraZ;
-	}
-	if (mInput.Key('S'))
-	{
-		move = move - cameraZ;
-	}
-	//移動あり
-	if (move.Length() > 0.0f)
-	{
-		//遊び
-		const float MARGIN = 0.06f;
-		//正規化
-		move = move.Normalize();
-		//自分の向きと向かせたい向きで外積
-		float cross = charZ.Cross(move).Y();
-		//自分の向きと向かせたい向きで内積
-		float dot = charZ.Dot(move);
-		//外積がプラスは左回転
-		if (cross > MARGIN)
-		{
-			mRotation.Y(mRotation.Y() + 5.0f);
-		}
-		//外積がマイナスは右回転
-		else if (cross < -MARGIN)
-		{
-			mRotation.Y(mRotation.Y() - 5.0f);
-		}
-		//前後の向きが同じとき内積は1.0
-		else if (dot < 1.0f - MARGIN)
-		{
-			mRotation.Y(mRotation.Y() - 5.0f);
-		}
-		//移動方向へ移動
-		mPosition = mPosition + move * 0.1f;
-		ChangeAnimation(1, true, 60);
-	}
-
 	if (!isAttack)
 	{
+		/*
 		//printf("攻撃中では有りません\n");
 		//Y軸+ 左へ2度回転
 		if (mInput.Key('A'))
@@ -91,10 +33,8 @@ void CXPlayer::Update()
 			//Z軸方向の値を回転させ移動させる
 			mPosition = mPosition + CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
 		}
-		else if (!mInput.Key('W') && AnimationIndex() == 1)
-		{
-			ChangeAnimation(0, true, 60);
-		}
+		
+		*/
 
 		if (mInput.Key(VK_SPACE))
 		{
@@ -102,6 +42,72 @@ void CXPlayer::Update()
 				return;
 			isAttack = true;
 			ChangeAnimation(3, false, 30);
+		}
+
+		// 課題30ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+		//カメラの前方
+		CVector cameraZ = CActionCamera::Instance()->VectorZ();
+		//カメラの左方向
+		CVector cameraX = CActionCamera::Instance()->VectorX();
+		//キャラクタの前方
+		CVector charZ = mMatrixRotate.VectorZ();
+		//XZ平面にして正規化
+		cameraZ.Y(0.0f); cameraZ = cameraZ.Normalize();
+		cameraX.Y(0.0f); cameraX = cameraX.Normalize();
+		charZ.Y(0.0f); charZ = charZ.Normalize();
+		//移動方向の設定
+		CVector move;
+		if (mInput.Key('A'))
+		{
+			move = move + cameraX;
+		}
+		if (mInput.Key('D'))
+		{
+			move = move - cameraX;
+		}
+		if (mInput.Key('W'))
+		{
+			move = move + cameraZ;
+		}
+		if (mInput.Key('S'))
+		{
+			move = move - cameraZ;
+		}
+		//移動あり
+		if (move.Length() > 0.0f)
+		{
+			//遊び
+			const float MARGIN = 0.06f;
+			//正規化
+			move = move.Normalize();
+			//自分の向きと向かせたい向きで外積
+			float cross = charZ.Cross(move).Y();
+			//自分の向きと向かせたい向きで内積
+			float dot = charZ.Dot(move);
+			//外積がプラスは左回転
+			if (cross > MARGIN)
+			{
+				mRotation.Y(mRotation.Y() + 5.0f);
+			}
+			//外積がマイナスは右回転
+			else if (cross < -MARGIN)
+			{
+				mRotation.Y(mRotation.Y() - 5.0f);
+			}
+			//前後の向きが同じとき内積は1.0
+			else if (dot < 1.0f - MARGIN)
+			{
+				mRotation.Y(mRotation.Y() - 5.0f);
+			}
+			//移動方向へ移動
+			mPosition = mPosition + move * 0.1f;
+			ChangeAnimation(1, true, 60);
+		}
+		// ここ少し変えました
+		else if (move.Length() == 0.0f && AnimationIndex() == 1)
+		{
+			ChangeAnimation(0, true, 60);
 		}
 	}
 	else if (isAttack)
