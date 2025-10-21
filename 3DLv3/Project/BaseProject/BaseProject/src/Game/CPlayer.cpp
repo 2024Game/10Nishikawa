dc266d122ab65dab7198e89b9b6d629463b408c3
Mao-Nishikawa-Fukuyama <gc24014@anabuki-net.ne.jp>
2025-10-21 15:02:16 +0900

    25/10/21

---
+++ 3DLv3/Project/BaseProject/BaseProject/src/Game/CPlayer.cpp
@@ -0,0 +1,257 @@
+//プレイヤークラスのインクルード
+#include "CPlayer.h"
+#include "CInput.h"
+#include "CCamera.h"
+#include "Maths.h"
+#include "CColliderCapsule.h"
+#include "CColliderSphere.h"
+
+#define BODY_HEIGHT 16.0f	// 本体のコライダーの高さ
+#define BODY_RADIUS 3.0f	// 本体のコライダーの幅
+#define MOVE_SPEED 0.75f	// 移動速度
+
+// プレイヤーのインスタンス
+CPlayer* CPlayer::spInstance = nullptr;
+
+// コンストラクタ
+CPlayer::CPlayer()
+	: CCharaBase(ETag::ePlayer, ETaskPriority::ePlayer)
+	, mState(EState::eIdle)
+	, mStateStep(0)
+	, mElapsedTime(0.0f)
+	, mMoveSpeedY(0.0f)
+	,mpModel(nullptr)
+	,mpBodyCol(nullptr)
+{
+	mMaxHp = 100000;
+	mHp = mMaxHp;
+
+	//インスタンスの設定
+	spInstance = this;
+
+	// モデルデータ取得
+	mpModel = CResourceManager::Get<CModel>("Player");
+
+	// 本体のコライダーを作成
+	mpBodyCol = new CColliderCapsule
+	(
+		this, ELayer::ePlayer,
+		CVector(0.0f, BODY_RADIUS, 0.0f),
+		CVector(0.0f, BODY_HEIGHT - BODY_RADIUS, 0.0f),
+		BODY_RADIUS
+	);
+	mpBodyCol->SetCollisionTags({ ETag::eField, ETag::eRideableObject, ETag::eEnemy });
+	mpBodyCol->SetCollisionLayers({ ELayer::eField, ELayer::eEnemy, ELayer::eAttackCol });
+}
+
+CPlayer::~CPlayer()
+{
+	// コライダーを削除
+	SAFE_DELETE(mpBodyCol);
+}
+
+CPlayer* CPlayer::Instance()
+{
+	return spInstance;
+}
+
+// 状態を切り替え
+void CPlayer::ChangeState(EState state)
+{
+	if (mState == state) return;
+
+	mState = state;
+	mStateStep = 0;
+	mElapsedTime = 0.0f;
+}
+
+// 待機
+void CPlayer::UpdateIdle()
+{
+	
+}
+
+// 仰け反り
+void CPlayer::UpdateHit()
+{
+	switch (mStateStep)
+	{
+		case 0:
+			mStateStep++;
+			break;
+		case 1:
+			// 待機状態へ移行
+			ChangeState(EState::eIdle);
+			break;
+	}
+}
+
+// オブジェクト削除を伝える
+void CPlayer::DeleteObject(CObjectBase* obj)
+{
+	
+}
+
+// キーの入力情報から移動ベクトルを求める
+CVector CPlayer::CalcMoveVec() const
+{
+	CVector move = CVector::zero;
+
+	// キーの入力ベクトルを取得
+	CVector input = CVector::zero;
+	if (CInput::Key('W'))		input.Y(-1.0f);
+	else if (CInput::Key('S'))	input.Y(1.0f);
+	if (CInput::Key('A'))		input.X(-1.0f);
+	else if (CInput::Key('D'))	input.X(1.0f);
+
+	// 入力ベクトルの長さで入力されているか判定
+	if (input.LengthSqr() > 0.0f)
+	{
+		// 上方向ベクトル
+		CVector up = CVector::up;
+		// カメラの向きに合わせた移動ベクトルに変換
+		CCamera* mainCamera = CCamera::MainCamera();
+		CVector camForward = mainCamera->VectorZ();
+		camForward.Y(0.0f);
+		camForward.Normalize();
+		// カメラの正面方向ベクトルと上方向ベクトルの外積から
+		// 横方向の移動ベクトルを求める
+		CVector moveSide = CVector::Cross(up, camForward);
+		// 横方向の移動ベクトルと上方向ベクトルの外積から
+		// 正面方向の移動ベクトルを求める
+		CVector moveForward = CVector::Cross(moveSide, up);
+
+		// 求めた各方向の移動ベクトルから、
+		// 最終的なプレイヤーの移動ベクトルを求める
+		move = moveForward * input.Y() + moveSide * input.X();
+		move.Normalize();
+	}
+
+	return move;
+}
+
+// 移動の更新処理
+void CPlayer::UpdateMove()
+{
+	mMoveSpeed = CVector::zero;
+
+	// プレイヤーの移動ベクトルを求める
+	CVector move = CalcMoveVec();
+	// 求めた移動ベクトルの長さで入力されているか判定
+	if (move.LengthSqr() > 0.0f)
+	{
+		mMoveSpeed += move * MOVE_SPEED;
+	}
+}
+
+// 更新
+void CPlayer::Update()
+{
+	// 状態に合わせて、更新処理を切り替える
+	switch (mState)
+	{
+		// 待機状態
+		case EState::eIdle:			UpdateIdle();		break;
+		// 仰け反り
+		case EState::eHit:			UpdateHit();		break;
+	}
+
+	// 待機中とは、移動処理を行う
+	if (mState == EState::eIdle)
+	{
+		UpdateMove();
+	}
+
+	// 移動
+	Position(Position() + mMoveSpeed);
+
+	// プレイヤーを移動方向へ向ける
+	CVector current = VectorZ();
+	CVector target = mMoveSpeed;
+	target.Y(0.0f);
+	target.Normalize();
+	CVector forward = CVector::Slerp(current, target, 0.125f);
+	Rotation(CQuaternion::LookRotation(forward));
+
+	CVector pos = Position();
+	CDebugPrint::Print("PlayerHP:%d / %d\n", mHp, mMaxHp);
+	CDebugPrint::Print("PlayerPos:%.2f, %.2f, %.2f\n", pos.X(), pos.Y(), pos.Z());
+	CDebugPrint::Print("PlayerState:%d\n", mState);
+
+	CDebugPrint::Print("FPS:%f\n", Times::FPS());
+}
+
+// ダメージを受ける
+void CPlayer::TakeDamage(int damage, CObjectBase* causer)
+{
+	// ベースクラスのダメージ処理を呼び出す
+	CCharaBase::TakeDamage(damage, causer);
+
+	// 死亡していなければ、
+	if (!IsDeath())
+	{
+		// 仰け反り状態へ移行
+		ChangeState(EState::eHit);
+
+		// 移動を停止
+		mMoveSpeed = CVector::zero;
+	}
+}
+
+// 衝突処理
+void CPlayer::Collision(CCollider* self, CCollider* other, const CHitInfo& hit)
+{
+	// 本体のコライダーの衝突判定
+	if (self == mpBodyCol)
+	{
+		// フィールドとの衝突
+		if (other->Layer() == ELayer::eField)
+		{
+			// 坂道で滑らないように、押し戻しベクトルのXとZの値を0にする
+			CVector adjust = hit.adjust;
+			adjust.X(0.0f);
+			adjust.Z(0.0f);
+
+			Position(Position() + adjust * hit.weight);
+
+			// 衝突した地面が床か天井かを内積で判定
+			CVector normal = hit.adjust.Normalized();
+			float dot = CVector::Dot(normal, CVector::up);
+			// 内積の結果がプラスであれば、床と衝突した
+			if (dot >= 0.0f)
+			{
+				// 落下などで床に上から衝突した時（下移動）のみ
+				// 上下の移動速度を0にする
+				if (mMoveSpeedY < 0.0f)
+				{
+					mMoveSpeedY = 0.0f;
+				}
+			}
+			// 内積の結果がマイナスであれば、天井と衝突した
+			else
+			{
+				// ジャンプなどで天井に下から衝突した時（上移動）のみ
+				// 上下の移動速度を0にする
+				if (mMoveSpeedY > 0.0f)
+				{
+					mMoveSpeedY = 0.0f;
+				}
+			}
+		}
+		// 敵と衝突した場合
+		else if (other->Layer() == ELayer::eEnemy)
+		{
+			// 横方向にのみ押し戻すため、
+			// 押し戻しベクトルのYの値を0にする
+			CVector adjust = hit.adjust;
+			adjust.Y(0.0f);
+			Position(Position() + adjust * hit.weight);
+		}
+	}
+}
+
+// 描画
+void CPlayer::Render()
+{
+	mpModel->Render(Matrix());
+}
---
+++ 3DLv3/Project/BaseProject/BaseProject/src/Game/CPlayer.h
@@ -0,0 +1,76 @@
+#pragma once
+//キャラクタクラスのインクルード
+#include "CCharaBase.h"
+#include "CModel.h"
+
+class CCollider;
+
+/*
+プレイヤークラス
+キャラクタクラスを継承
+*/
+class CPlayer : public CCharaBase
+{
+public:
+	//インスタンスのポインタの取得
+	static CPlayer* Instance();
+
+	// コンストラクタ
+	CPlayer();
+	// デストラクタ
+	~CPlayer();
+
+	// 更新
+	void Update();
+
+	// ダメージを受ける
+	void TakeDamage(int damage, CObjectBase* causer) override;
+
+	/// <summary>
+	/// 衝突処理
+	/// </summary>
+	/// <param name="self">衝突した自身のコライダー</param>
+	/// <param name="other">衝突した相手のコライダー</param>
+	/// <param name="hit">衝突した時の情報</param>
+	void Collision(CCollider* self, CCollider* other, const CHitInfo& hit) override;
+
+	// 描画
+	void Render();
+
+private:
+	// オブジェクト削除を伝える
+	void DeleteObject(CObjectBase* obj) override;
+
+	// キーの入力情報から移動ベクトルを求める
+	CVector CalcMoveVec() const;
+
+	// 待機状態
+	void UpdateIdle();
+	// 仰け反り
+	void UpdateHit();
+
+	// 移動の更新処理
+	void UpdateMove();
+
+	// プレイヤーのインスタンス
+	static CPlayer* spInstance;
+
+	// プレイヤーの状態
+	enum class EState
+	{
+		eIdle,		// 待機
+		eHit,		// 仰け反り
+	};
+	// 状態を切り替え
+	void ChangeState(EState state);
+
+	EState mState;				// プレイヤーの状態
+	int mStateStep;				// 状態内のステップ管理用
+	float mElapsedTime;			// 経過時間計測用
+
+	CVector mMoveSpeed;			// 前後左右の移動速度
+	float mMoveSpeedY;			// 重力やジャンプによる上下の移動速度
+
+	CModel* mpModel;			// 本体のモデルデータ
+	CCollider* mpBodyCol;		// 本体のコライダー
+};
